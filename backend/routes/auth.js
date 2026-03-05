3c60fbbed4f42d12b8c2f095bbe0343fd2974519
Retr-04 <tonystephen877@gmail.com>
2026-03-05 12:34:43 +0300

    bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,50 +1,57 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require(".../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 
-// register 
+/* REGISTER */
 router.post("/register", async (req, res) => {
     try {
-        const {name, email, password, role} = req.body;
+        const { name, email, password, role } = req.body;
 
         const hashPassword = await bcrypt.hash(password, 10);
 
         const newUser = new User({
             name,
             email,
-            password: hashPassword, 
+            password: hashPassword,
             role
         });
 
         await newUser.save();
-        
-        res.status(201).json({ message: "User registered successfully"});
+
+        res.status(201).json({ message: "User registered successfully" });
+
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
-    });
+});
 
-// login
+/* LOGIN */
 router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const user = await User.findOne({ email});
-        if (!user) return res.status(400).json({ error: "user not found"});
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ error: "User not found" });
+        }
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json({ message: "invalid password"});
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
 
         const token = jwt.sign(
             { id: user._id, role: user.role },
             "SECRETKEY",
             { expiresIn: "1d" }
         );
-        
-        res.json({ 
+
+        res.json({
             token,
             user: {
                 name: user.name,
@@ -54,10 +61,8 @@ router.post("/login", async (req, res) => {
         });
 
     } catch (err) {
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
     }
-    
 });
 
 module.exports = router;
-

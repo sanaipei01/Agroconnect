b0f7a835b81053cf56e3265e0aa40d00f0ea8c97
Retr-04 <tonystephen877@gmail.com>
2026-03-17 07:10:34 +0300

    backend serves frontend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 mongoose.connect("mongodb://127.0.0.1:27017/agrisystem")
     .then(() => console.log("MongoDB connected"))
